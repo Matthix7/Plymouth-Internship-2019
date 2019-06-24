1d6435b67fcd2f9dab1aef7a8f9f4ff0ce41dd29
Matthix7 <catamatth@gmail.com>
2019-06-24 15:04:22 +0100

    Test camera

--- src/PiCamera/testCamera.py
+++ src/PiCamera/testCamera.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 import rospy
 
+# import the necessary packages
+from picamera.array import PiRGBArray
 from picamera import PiCamera
-from time import sleep
+import time
+import cv2
 
-def run():
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+rawCapture = PiRGBArray(camera)
 
-    camera = PiCamera()
+# allow the camera to warmup
+time.sleep(0.1)
 
-    camera.start_preview()
-    sleep(10)
-    camera.stop_preview()
+# grab an image from the camera
+camera.capture(rawCapture, format="bgr")
+image = rawCapture.array
+
+# display the image on screen and wait for a keypress
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
