d4409ae9eb6c8b9dd8d9f1624858f1551dc913fd
Matthix7 <catamatth@gmail.com>
2019-07-04 14:07:45 +0100

    ArUco detection complete

--- src/PiCamera/ArUcoDetection.py
+++ src/PiCamera/ArUcoDetection.py
@@ -4,6 +4,8 @@
 ##import rospy
 
 # import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
 import time
 import cv2
 from cv2 import aruco
@@ -27,7 +29,7 @@ def run():
 
     t0 = time.time()
 
-    cv2.namedWindow('Webcam', cv2.WINDOW_AUTOSIZE)
+    cv2.namedWindow('Webcam', cv2.WINDOW_NORMAL)
 
     aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
 
@@ -43,28 +45,28 @@ def run():
 
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-
         parameters =  aruco.DetectorParameters_create()
         corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
-        frame_markers = aruco.drawDetectedMarkers(image.copy(), corners, ids)
+        frame_markers = aruco.drawDetectedMarkers(image, corners, ids)
+
+        if ids is not None:
+
+            markerLength = 3.8
+            camera_matrix = array([[485.36568341, 0, 308.96642615], [0, 486.22575965, 236.66818825], [0, 0, 1]])
+            dist_coeffs = array([[1.37958351e-01, -2.43061015e-01, -5.22562568e-05, -6.84849581e-03, -2.59284496e-02]])
+
+            rvec, tvec = aruco.estimatePoseSingleMarkers(corners, markerLength, camera_matrix, dist_coeffs) # For a single marker
+
+            
+            for i in range(len(ids)):
+                frame_markers = aruco.drawAxis(frame_markers, camera_matrix, dist_coeffs, rvec[i], tvec[i], 10)
+
+        frame_markers = cv2.flip(frame_markers, 0)
+        cv2.imshow("Webcam", frame_markers)
 
 
 
-        markerLength = 3.8
-        camera_matrix = array([[485.36568341, 0, 308.96642615], [0, 486.22575965, 236.66818825], [0, 0, 1]])
-        dist_coeffs = array([[1.37958351e-01, -2.43061015e-01, -5.22562568e-05, -6.84849581e-03, -2.59284496e-02]])
-
-        rvec, tvec = aruco.estimatePoseSingleMarkers(corners, markerLength, camera_matrix, dist_coeffs) # For a single marker
-
-        for i in range(len(ids)):
-            imgWithAruco = aruco.drawAxis(frame_markers, camera_matrix, dist_coeffs, rvec[i], tvec[i], 10)
-
-
-        cv2.imshow("Webcam", imgWithAruco)
-
-
-
-        time.sleep(0.1)
+##        time.sleep(0.1)
 
         key = cv2.waitKey(1) & 0xFF
         if key == 27 or key == ord('q'):
@@ -76,21 +78,21 @@ def run():
                 key = cv2.waitKey(1) & 0xFF
 
         elif key == ord('c'):
-            cv2.imwrite('sample.png',masts)
+            cv2.imwrite('sample_aruco.png',frame_markers)
             print("Picture saved")
 
 
-    cap.release()
     cv2.destroyAllWindows()
     print("Total time : ",time.time()-t0)
     print("Computed frames : ", c)
-    print("Time per frame : ", (time.time()-t0)/c - 0.1)
+    print("Time per frame : ", (time.time()-t0)/c) # - 0.1)
 
 
 
 
 
-
+if __name__ == "__main__":
+    run()
 
 
 
