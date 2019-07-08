d0fc9e1abbabc3edc7e50914f432d54d923c99b3
Matthix7 <catamatth@gmail.com>
2019-07-08 17:06:24 +0100

    Centralised image processing calls

--- src/PiCamera/detectionHorizonMast.py
+++ src/PiCamera/detectionHorizonMast.py
@@ -83,16 +83,15 @@ def run():
 def horizonArea(image, horizon_prev):
 
     grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    tTest = time.time()
 
 #    grey = cv2.bilateralFilter(grey,9,40,10) #more precision, but slower than medianBlur
 #    grey = cv2.medianBlur(grey,7)
-    print('T_test', time.time()-tTest)
 
 
 #    cv2.imshow('Blur', grey)
 
     rows,cols = grey.shape
+    tTest = time.time()
 
     kernel = np.zeros((7,7))
     kernel_side = np.ones((3,7))
@@ -100,6 +99,7 @@ def horizonArea(image, horizon_prev):
     kernel[-3:] = (1./(4*7))*kernel_side
     grad_y = cv2.filter2D(grey,cv2.CV_16S,kernel)
     grad_y = np.uint8(np.absolute(grad_y))
+    print('T_test', time.time()-tTest)
 
 
     ret, bin_y = cv2.threshold(grad_y,10,255,0)
