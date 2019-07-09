17e85bdcfb2af5dee19c1ffe036638f1819b8006
Matthix7 <catamatth@gmail.com>
2019-07-09 10:49:03 +0100

    Centralised image processing calls

--- src/PiCamera/imageProcessing.py
+++ src/PiCamera/imageProcessing.py
@@ -61,7 +61,7 @@ def run():
     camera.resolution = (640, 480)
     camera.framerate = 60
 
-    camera.exposure_mode = 'sports'
+#    camera.exposure_mode = 'sports'
 
     rawCapture = PiRGBArray(camera, size=(640, 480))
 
