a14f804e5fbdc736ced8bbc6763ee8f3b3aaa940
Matthix7 <catamatth@gmail.com>
2019-07-09 10:00:47 +0100

    Centralised image processing calls

--- src/PiCamera/imageProcessing.py
+++ src/PiCamera/imageProcessing.py
@@ -94,7 +94,7 @@ def run():
 #        #horizon_prev: vertical position in pixels of the horizon in the previous uncropped image, in case horizon is not
 #        #detected in the new image.
         t1 = time.time()
-#        horizon, horizon_height, horizon_prev = horizonArea(image, horizon_prev)
+        horizon, horizon_height, horizon_prev = horizonArea(image, horizon_prev)
 
         cv2.imshow('Global', image)
 #        print('T1', time.time()-t1)
