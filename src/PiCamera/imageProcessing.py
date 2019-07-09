ceba686e701a72b85486f012508395c9fd002984
Matthix7 <catamatth@gmail.com>
2019-07-09 10:30:39 +0100

    Centralised image processing calls

--- src/PiCamera/imageProcessing.py
+++ src/PiCamera/imageProcessing.py
@@ -59,7 +59,7 @@ def run():
 #    Running with the camera
     camera = PiCamera()
     camera.resolution = (640, 480)
-    camera.framerate = 9
+    camera.framerate = 18
 
     camera.exposure_mode = 'sports'
 
