12dae25a04c73c47d0949d68b3facb762bd2aa10
Matthix7 <catamatth@gmail.com>
2019-07-09 09:29:41 +0100

    Centralised image processing calls

--- src/PiCamera/imageProcessing.py
+++ src/PiCamera/imageProcessing.py
@@ -33,93 +33,93 @@ def run():
 
     c = 0
 
-##############          VIDEO           #############################
-#####################################################################
-#    Running on test video
-    cap = cv2.VideoCapture('testImages/some_boats.mp4')
-
-    t0 = time.time()
-
-    dodo = 0.1
-
-    while(cap.isOpened()):
-
-        # Capture frame-by-frame
-        ret, image = cap.read()
-
-        if not ret:
-            break
-
-        image = cv2.resize(image, (640,480))
-
-
-
-##################     CAMERA     ####################################
+###############          VIDEO           #############################
 ######################################################################
-##    Running with the camera
-#    camera = PiCamera()
-#    camera.resolution = (640, 480)
-#    camera.framerate = 32
-
-#    camera.exposure_mode = 'sports'
-
-#    rawCapture = PiRGBArray(camera, size=(640, 480))
-
-#    # allow the camera to warmup
-#    time.sleep(0.1)
+##    Running on test video
+#    cap = cv2.VideoCapture('testImages/some_boats.mp4')
 
 #    t0 = time.time()
 
-#    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+#    dodo = 0.1
 
-#        image = frame.array
+#    while(cap.isOpened()):
 
-#        # clear the stream in preparation for the next frame
-#        rawCapture.truncate(0)
+#        # Capture frame-by-frame
+#        ret, image = cap.read()
+
+#        if not ret:
+#            break
+
+#        image = cv2.resize(image, (640,480))
+
+
+
+#################     CAMERA     ####################################
 #####################################################################
-#####################################################################
+#    Running with the camera
+    camera = PiCamera()
+    camera.resolution = (640, 480)
+    camera.framerate = 32
+
+    camera.exposure_mode = 'sports'
+
+    rawCapture = PiRGBArray(camera, size=(640, 480))
+
+    # allow the camera to warmup
+    time.sleep(0.1)
+
+    t0 = time.time()
+
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+
+        image = frame.array
+
+        # clear the stream in preparation for the next frame
+        rawCapture.truncate(0)
+######################################################################
+######################################################################
 
 
-        c += 1
+#        c += 1
 
-#        cv2.imshow('Origin', image)
+##        cv2.imshow('Origin', image)
 
-        #Find the area where horizon is located and return a frame containing the horizon, transformed to be horizontal.
-        #Takes about 0.04s per frame.
-        #horizon: image cropped around the horizon
-        #horizon_height: vertical position in pixels of the horizon in the cropped image (for masts detection)
-        #horizon_prev: vertical position in pixels of the horizon in the previous uncropped image, in case horizon is not
-        #detected in the new image.
-        t1 = time.time()
-        horizon, horizon_height, horizon_prev = horizonArea(image, horizon_prev)
-        print('T1', time.time()-t1)
+#        #Find the area where horizon is located and return a frame containing the horizon, transformed to be horizontal.
+#        #Takes about 0.04s per frame.
+#        #horizon: image cropped around the horizon
+#        #horizon_height: vertical position in pixels of the horizon in the cropped image (for masts detection)
+#        #horizon_prev: vertical position in pixels of the horizon in the previous uncropped image, in case horizon is not
+#        #detected in the new image.
+#        t1 = time.time()
+#        horizon, horizon_height, horizon_prev = horizonArea(image, horizon_prev)
+#        print('T1', time.time()-t1)
 
-        #Find the areas where vertical lines are found (ie possible sailboats).
-        #Takes about 0.1s per frame.
-        #masts: image cropped around the horizon, where vertical lines are highlighted
-        t2 = time.time()
-        masts = detectMast(horizon, horizon_height)
-        print('T2', time.time()-t2)
+#        #Find the areas where vertical lines are found (ie possible sailboats).
+#        #Takes about 0.1s per frame.
+#        #masts: image cropped around the horizon, where vertical lines are highlighted
+#        t2 = time.time()
+#        masts = detectMast(horizon, horizon_height)
+#        print('T2', time.time()-t2)
 
-        #Find the buoy in the cropped image and highlight them in the result image
-        t3 = time.time()
-        colorRange = getColorRange()
-        center, buoy = detectBuoy(image, image.copy(), colorRange)
-        print('T3', time.time()-t3)
+#        #Find the buoy in the cropped image and highlight them in the result image
+#        t3 = time.time()
+#        colorRange = getColorRange()
+#        center, buoy = detectBuoy(image, image.copy(), colorRange)
+#        print('T3', time.time()-t3)
 
-        #Find the April Tags in the cropped image
-        t4 = time.time()
-        frame_markers, corners = detectAruco(image, buoy, aruco_dict)
-        print('T4', time.time()-t4)
+#        #Find tarkers, corners = detectAruco(image, buoy, aruco_dict)
+#        print('T4', time.time()-t4)
+
+#        he April Tags in the cropped image
+#                t4 = time.time()
+#                frame_m
+#        t5 = time.time()
+        cv2.imshow('Global', image)
+#        cv2.imshow('Horizon', masts)
+#        print('T5', time.time()-t5)
 
 
-        t5 = time.time()
-        cv2.imshow('Global', frame_markers)
-        cv2.imshow('Horizon', masts)
-        print('T5', time.time()-t5)
-
-
-        time.sleep(dodo)
+#        time.sleep(dodo)
 
 #####################################################################
 #############        INTERACTION          ###########################
