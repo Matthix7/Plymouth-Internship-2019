fd5cd8489f4272580c02564b846f46a5d7de44da
Matthix7 <catamatth@gmail.com>
2019-07-11 14:45:04 +0100

    Reorganisation and target heading

--- src/PiCamera/chessboard_calibration.py
+++ src/PiCamera/chessboard_calibration.py
@@ -58,61 +58,61 @@ def getCamDistortData(filename):
 
 
 
+if __name__ == "__main__":
+    # termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-# termination criteria
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((6*9,3), np.float32)
+    objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
 
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*9,3), np.float32)
-objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+    # Arrays to store object points and image points from all the images.
+    objpoints = [] # 3d point in real world space
+    imgpoints = [] # 2d points in image plane.
 
-# Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
+    images = glob.glob('calibration_images/*.png')
+    count = 0
 
-images = glob.glob('calibration_images/*.png')
-count = 0
+    for fname in images:
+        img = cv2.imread(fname)
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-for fname in images:
-    img = cv2.imread(fname)
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        # Find the chess board corners
+        ret, corners = cv2.findChessboardCorners(gray, (9,6),None)
 
-    # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (9,6),None)
+        # If found, add object points, image points (after refining them)
+        if ret == True:
+            count += 1
+            objpoints.append(objp)
 
-    # If found, add object points, image points (after refining them)
-    if ret == True:
-        count += 1
-        objpoints.append(objp)
+            corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+            imgpoints.append(corners2)
 
-        corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-        imgpoints.append(corners2)
+            # Draw and display the corners
+            img = cv2.drawChessboardCorners(img, (9,6), corners2,ret)
+            cv2.imshow('img',img)
+            cv2.waitKey(500)
 
-        # Draw and display the corners
-        img = cv2.drawChessboardCorners(img, (9,6), corners2,ret)
-        cv2.imshow('img',img)
-        cv2.waitKey(500)
+    cv2.destroyAllWindows()
 
-cv2.destroyAllWindows()
+    print("Found ", count, "correct images.")
 
-print("Found ", count, "correct images.")
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
 
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
-
-saveFile = open("calibration_data.txt", 'w')
-saveFile.write("#ret" +'\n'+ str(ret) +'\n'+ '#mtx'+'\n'+ str(mtx) +'\n'+ '#dist'+'\n'+ str(dist) +'\n'+ '#rvecs'+'\n'+ str(rvecs) +'\n'+ '#tvecs'+'\n'+str(tvecs))
-saveFile.close()
-
-
-img = cv2.imread('calibration_images/fra35.png')
-h,  w = img.shape[:2]
+    saveFile = open("calibration_data.txt", 'w')
+    saveFile.write("#ret" +'\n'+ str(ret) +'\n'+ '#mtx'+'\n'+ str(mtx) +'\n'+ '#dist'+'\n'+ str(dist) +'\n'+ '#rvecs'+'\n'+ str(rvecs) +'\n'+ '#tvecs'+'\n'+str(tvecs))
+    saveFile.close()
 
 
-# undistort
-dst = cv2.undistort(img, mtx, dist, None)
+    img = cv2.imread('calibration_images/fra35.png')
+    h,  w = img.shape[:2]
 
 
-cv2.imwrite('calibration_images/calibresult_fra35.png',dst)
+    # undistort
+    dst = cv2.undistort(img, mtx, dist, None)
+
+
+    cv2.imwrite('calibration_images/calibresult_fra35.png',dst)
 
 
 
