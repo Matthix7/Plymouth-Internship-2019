531eb99deea70a8e6ada4fd0d06326aa0c2366ba
Matthix7 <catamatth@gmail.com>
2019-07-19 11:04:00 +0100

    Testing generalised communications

--- src/Communications/fleetCoordinator.py
+++ src/Communications/fleetCoordinator.py
@@ -111,7 +111,7 @@ def is_valid(line):
 
 def run():
     expected_fleet_size = 2
-    receiving_freq = 5. #Set the speed of the transmission loops
+    receiving_freq = 10. #Set the speed of the transmission loops
 
 ###################################################################################################
 #    Look for XBee USB port, to avoid conflicts with other USB devices
