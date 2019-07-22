85ff5fd3ec7c5a62158e255a98e565d8cebf936f
Matthix7 <catamatth@gmail.com>
2019-07-22 12:23:44 +0100

    Improving generalised communications.

--- src/Communications/fleetCoordinator.py
+++ src/Communications/fleetCoordinator.py
@@ -249,9 +249,10 @@ def run():
         while c!= '#' and (time()-loopTime)<(1/receiving_freq) and not rospy.is_shutdown():
             c = ser.read(1)
         msgTime = time()
-        while c!='=' and (time()-msgTime)<(1/receiving_freq) and not rospy.is_shutdown():
-            c = ser.read(1)
-            line += c
+        if c == '#':
+            while c != '=' and (time()-msgTime)<(1/receiving_freq) and not rospy.is_shutdown():
+                c = ser.read(1)
+                line += c
 
         if "Hello" in line:
             continue
--- src/Communications/fleetSailboat.py
+++ src/Communications/fleetSailboat.py
@@ -310,10 +310,10 @@ def run():
         while c != '#' and (time()-loopTime)<(1/receiving_freq) and not rospy.is_shutdown():
             c = ser.read(1)
         msgTime = time()
-
-        while c != '=' and (time()-msgTime)<(1/receiving_freq) and not rospy.is_shutdown():
-            c = ser.read(1)
-            line += c
+        if c == '#':
+            while c != '=' and (time()-msgTime)<(1/receiving_freq) and not rospy.is_shutdown():
+                c = ser.read(1)
+                line += c
 
         # Check message syntax and checkSum and clean the message to use only the useful data
         check, msgReceived = is_valid(line)
