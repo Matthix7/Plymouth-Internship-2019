27c9b416c54762f86b78bc49dfb517488e1910fc
Matthix7 <catamatth@gmail.com>
2019-08-09 11:18:50 +0100

    Launch at boot

---
+++ Launch/fleetSailboat.bash
@@ -0,0 +1,4 @@
+source /opt/ros/melodic/setup.bash
+source ~/workspaceRos/devel/setup.bash
+source ~/Documents/Annee_2/Stage_Plymouth/workspaceRos/devel/setup.bash
+roslaunch plymouth_internship_2019 fleetSailboat.launch
