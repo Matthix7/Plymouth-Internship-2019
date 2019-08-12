365b1d512364304c08373a2041ef58d3e767c99e
Matthix7 <catamatth@gmail.com>
2019-08-12 15:28:17 +0100

    Connection at boot

--- Launch/fleetSailboat.bash
+++ Launch/fleetSailboat.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+echo "`date +%s`" > /home/sailboats/toto.txt
+sleep 30
 source /opt/ros/melodic/setup.bash
 source /home/sailboats/workspaceRos/devel/setup.bash
-
+echo "`date +%s`" >> /home/sailboats/toto.txt
 roslaunch plymouth_internship_2019 sailboatBoot.launch
